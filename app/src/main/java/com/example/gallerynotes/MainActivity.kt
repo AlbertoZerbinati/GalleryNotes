0e2d2f155dc943118bab6d633bdf81e5c6d6d5c6
Alberto Zerbinati <zerbi.albi@gmail.com>
2021-05-20 16:38:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gallerynotes/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.gallerynotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    private val RC_ADD_NOTE : Int= 1;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val addNoteImage : ImageView = findViewById(R.id.imageAddNote)
+        addNoteImage.setOnClickListener { view ->
+            val intent = Intent(view.context, CreateNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
